58b13542efd9231f49a5028571b168f27b358fe2
Dinusha <dinusha.16@itfac.mrt.ac.lk>
2020-06-17 22:42:56 +0530

    service component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default class Navbar extends Component {
   }
 
   handleToggle = () => {
-    this.setState({ isOpen: ! this.state.isOpen })
+    this.setState({ isOpen: !this.state.isOpen }) 
   }
 
   render() {
@@ -18,14 +18,14 @@ export default class Navbar extends Component {
       <nav className='navbar'>
         <div className='nav-center'>
           <div className='nav-header'>
-            <Link to = '/'>
+            <Link to='/'>
               <img src={logo} alt='Beach Resort Logo' />
             </Link>
             <button type='button' className='nav-btn' onClick={this.handleToggle}>
               <FaAlignRight className='nav-icon' />
             </button>
           </div>
-          <ul className={this.state.isOpen? 'nav-links show-nav' : 'nav-links'}>
+          <ul className={this.state.isOpen ? 'nav-links show-nav' : 'nav-links'}>
             <li>
               <Link to='/'>Home</Link>
             </li>
